ca6fd55c9b5f9f5e3b4c3fc4a75d5176c67427ce
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2025-01-17 14:29:52 +0200

    Fixing empty string

--- Task1LargestNumberInArray/Program.cs
+++ Task1LargestNumberInArray/Program.cs
@@ -1,27 +1,29 @@
 ﻿
-class Task1LargestNumberInArray {
+public class Task1LargestNumberInArray {
     static int Largest(int[] arr) {
         int max = arr[0];
-        
-        for (int i = 1; i < arr.Count(); i++)
-            if (arr[i] > max)
-                max = arr[i];
-        return max;
-        
-    }
 
+        for (int i = 1; i < arr.Count(); i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        return max;  
+    }
     static void Main() {
         Console.WriteLine("Enter an array of integers, separated by spaces:");
         string input = Console.ReadLine();
         
-        int[] arr = Array.ConvertAll(input.Split(' '), int.Parse);
         
-        if (arr.Length == 0)
+        if (string.IsNullOrWhiteSpace(input))
         {
             Console.WriteLine("The array is empty.");
         }
         else
         {
+            int[] arr = Array.ConvertAll(input.Split(), int.Parse);
             Console.WriteLine(Largest(arr));
         }
     }
