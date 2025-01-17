cb6fb1a11204cc1d3d4fa800fd93a6ea13e0528f
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2025-01-17 16:06:00 +0200

    Task2 Ready

---
+++ Task2ReverseAString/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+public class Task2ReverseAString
+{
+    public static string ReverseString(string input)
+    {
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter a string to reverse:");
+        string userInput = Console.ReadLine();
+        string reversedString = ReverseString(userInput);
+        Console.WriteLine("Reversed string: " + reversedString);
+    }
+}
