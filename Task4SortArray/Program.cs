032dfa46789ac1e5fe6dd76863cc8c735fdd6dab
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2025-01-29 14:58:58 +0200

    task 4, 5

---
+++ Task4SortArray/Program.cs
@@ -0,0 +1,32 @@
+﻿public class SortArray
+{
+    public static void SortAscending(int[] array)
+    {
+        int arrayLength = array.Length;
+        for (int i = 0; i < arrayLength - 1; i++)
+        {
+            for (int j = 0; j < arrayLength - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter the elements of the array, separated by spaces:");
+        string input = Console.ReadLine();
+        int[] array = input.Split(' ').Select(int.Parse).ToArray();
+        
+        Console.WriteLine("Original array: " + string.Join(", ", array));
+
+        SortAscending(array);
+
+        Console.WriteLine("Sorted array: " + string.Join(", ", array));
+    }
+}
---
+++ Task5SwapTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿public class SwapNumbers
+{
+    public static void Swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Before swap: x = " + x + ", y = " + y);
+
+        Swap(ref x, ref y);
+
+        Console.WriteLine("After swap: x = " + x + ", y = " + y);
+    }
+}
